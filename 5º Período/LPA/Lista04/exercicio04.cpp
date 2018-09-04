fd1d224d1b8c3dd93452f83591aa5f990c75070c
KakaSena <kaka@DESKTOP-JN4PBLJ.localdomain>
2018-09-04 23:21:58 +0000

    Update exercicio04

--- 5º Período/LPA/Lista04/exercicio04.cpp
+++ 5º Período/LPA/Lista04/exercicio04.cpp
@@ -4,18 +4,21 @@
 
 using namespace std;
 
+// metodo para facilitar leitura de inteiro
 int lerInteiro(){
     int x;
     cin >> x;  
     return x;
 }
 
+// metodo boolean para verficar se o teste executado pode ser aceitdo
 bool isPossivel(vector<string> vectorString)
 {
     bool resp = false;
     vector<char> vectorPrimeiroChar;
     vector<char> vectorUltimoChar;
  
+    // for que recebe o vectorString e pega a primeira e ultima letra de cada palavra de todas position do vetor
     for(int i = 0; i < vectorString.size(); i++){
 
         vectorPrimeiroChar.push_back(vectorString[i].at(0));
@@ -35,22 +38,25 @@ int main(){
     int t;
     int n;
 
+    // le t, que corresponde ao numero de teste
     t = lerInteiro();
     
+    //while para rodar todos T testes do arquivo
     while(t > 0){
 
         vector<string> vectorString;
 
+        // le n, que corresponde a quantidade de palavras no teste T
         n = lerInteiro();
-        cout << "N:" << n << endl;
-
+        
+        //for que percorre todas palavras do teste T e coloca na variavel vectorString
         for(int i = 0; i < n; i++){
             
             cin >> s;
-            cout << s.length();
+            
             vectorString.push_back(s);
 
-            cout << vectorString[i] << endl;
+            
             
         }
         t--;
